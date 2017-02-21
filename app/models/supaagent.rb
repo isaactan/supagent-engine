83ca7517e458dd752d6abd7a87c9a487eae0c242
isaac <isaac@supahands.com>
2017-02-21 09:06:48 +0000

    env-envrioment in model file

--- app/models/supaagent.rb
+++ app/models/supaagent.rb
@@ -10,5 +10,5 @@ class Supaagent < ActiveRecord::Base
     validates_presence_of :bankname, :message => "Bank Name is required"
     validates_presence_of :bankaddress, :message => "Bank Address is required"
     validates_presence_of :bankswift, :message => "Bank SWIFT code is required"
-    validates_presence_of :terms, :message => "checkbox"
+    validates_presence_of :terms, :message => "You forgot to tick the checkbox for Terms and Condition"
 end
