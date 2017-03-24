251006f503ae5c6c063559485f8212b8dface23d
isaac <isaac@supahands.com>
2017-03-24 07:33:57 +0000

    wice-grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.initialize_on_precompile = false
+
 
   # Do not dump schema after migrations.
 # do not 
